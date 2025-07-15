918a5f5649e4fed894471778f2f378df3a307c22
YbrDev-web <ybenromdhane2003@gmail.com>
2025-07-15 18:00:56 +0200

    css et page 404

---
+++ ESGI-Themes/templates/404.php
@@ -0,0 +1,21 @@
+<?php
+
+ /**
+ * Template Name: 404
+ */
+
+ get_header();
+ ?>
+
+<main class="page-404 full-blue">
+  <div class="container">
+    <h1>404 – Page Not Found</h1>
+    <p>Sorry, the page you're looking for doesn't exist or has been moved.</p>
+    <a href="<?php echo home_url(); ?>" class="btn-link">Back to Homepage</a>
+  </div>
+</main>
+
+<?php
+
+get_footer();
+?>
