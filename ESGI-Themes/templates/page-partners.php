30a2daf9a128877050b1ad95152f696e088de197
YbrDev-web <ybenromdhane2003@gmail.com>
2025-07-15 15:59:59 +0200

    page partners mise à jour

---
+++ ESGI-Themes/templates/page-partners.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Template Name: Partners
+ */
+get_header();
+?>
+
+<main class="partners-page container">
+  <h1>Our Partners</h1>
+  <p>They trust us. Discover who we proudly work with.</p>
+
+  <div class="logos">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/svg/partner-1.svg" alt="Partner">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/svg/partner-2.svg" alt="Partner">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/svg/partner-3.svg" alt="Partner">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/svg/partner-4.svg" alt="Partner">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/svg/partner-5.svg" alt="Partner">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/svg/partner-6.svg" alt="Partner">
+  </div>
+</main>
+
+<?php
+get_footer();
+?>
